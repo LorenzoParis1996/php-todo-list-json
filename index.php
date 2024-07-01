b2c8ad5f3d54abe25b67d73457f3d57e2ee56938
LorezoParis1996 <lorenzoparis86@live.it>
2024-07-01 14:19:19 +0200

    scaffolding

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.2/axios.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div id="app">
+
+    </div>
+    
+
+
+
+
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+</body>
+</html>
